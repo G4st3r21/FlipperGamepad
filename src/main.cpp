852f702deb6bd266e8c35d298a0673e6c5b236d9
G4st3r21 <aporyadin003@gmail.com>
2024-05-01 17:25:25 +0300

    rtc test (failed)

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,39 @@
 #include <Arduino.h>
+#include <RTClib.h>
 
-// put function declarations here:
-int myFunction(int, int);
+RTC_DS3231 rtc;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Wire.begin();
+
+  if (!rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+
+  if (rtc.lostPower()) {
+    Serial.println("RTC lost power, setting the time");
+    // rtc.adjust(DateTime(F(DATE), F(TIME)));
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  DateTime now = rtc.now();
+  
+  Serial.print("Date: ");
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+  Serial.print(" Time: ");
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.print(now.second(), DEC);
+  Serial.println();
+  
+  delay(1000);
 }
